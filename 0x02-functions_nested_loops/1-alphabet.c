6d4f39ab38c2026c451617b65eed55707f11cfb3
Yusuf Olamilekan Muktar <60045679+yusufom@users.noreply.github.com>
2022-06-08 21:26:36 +0100

    Update 1-alphabet.c

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -3,15 +3,15 @@
 * print_alphabet - prints all the english alphabets
 * in lower case
 *
-*Return: 0
+* Return: 0
 */
 void print_alphabet(void)
 {
   char i;
 
   for (i = 'a'; i <= 'z'; i++)
-{
-	_putchar(i);
-}
+	{
+		_putchar(i);
+	}
 	_putchar('\n');
 }
