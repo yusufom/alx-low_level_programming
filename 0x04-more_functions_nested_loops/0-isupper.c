1c0cd32aa9f15063b1c577cba59ba9ab2bd463b3
yusufom <yusufolamilekanmuktar@gmail.com>
2022-06-10 15:18:33 -0700

    functions

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+* _isupper - checks for uppercase character
+* @c: character to be checked
+* Return: 1 if upper case, 0 otherwise.
+*
+*/
+int _isupper(int c)
+{
+	return (c >= 'A' && c <= 'Z');
+}
---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+* _isdigit - checks if a character is digit or not.
+* @c: character to be checked
+* Return: 1 if digit, 0 otherwise.
+*
+*/
+int _isdigit(int c)
+{
+  return (c >= '0' && c <= '9');
+}
