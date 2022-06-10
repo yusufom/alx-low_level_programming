ad05bf5d45a4d1044cf6cc985e03d5bb58162e0c
yusufom <yusufolamilekanmuktar@gmail.com>
2022-06-10 16:26:11 -0700

    Hello

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,9 +1,9 @@
 #include "main.h"
 
 /**
- * print_numbers - prints the numbers, from 0 to 9,
- * followed by a new line
- */
+* print_numbers - prints the numbers, from 0 to 9,
+* followed by a new line
+*/
 void print_numbers(void)
 {
 	int i;
